73e6c455a01656098957f22c0c95d89f504485fd
steffenpedersen <steffen.pedersen@live.dk>
2021-10-25 19:16:08 +0200

    Add grid to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,35 @@ import styled from "styled-components";
 import Layout, { siteTitle } from "../components/layout";
 import { device, LinkGradient } from "../styles/components";
 
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column-reverse;
+
+  @media ${device.md} {
+    flex-direction: row;
+  }
+`;
+
 const Content = styled.div`
   font-size: 1.875rem;
   line-height: 2.25rem;
-  max-width: 42rem;
 
   @media ${device.md} {
+    flex: 3;
     margin-right: 2rem;
   }
 `;
 
 const ImageContent = styled.div`
-    display: flex;
-    justify-content: center;
-    flex: 1 1 0%;
+  display: flex;
+  justify-content: center;
 
   @media ${device.md} {
+    flex: 1;
     margin-left: 2rem;
   }
 `;
-
 
 export default function Home({ toggleTheme, isDarkTheme }) {
   return (
@@ -32,33 +41,34 @@ export default function Home({ toggleTheme, isDarkTheme }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-
-      <Content>
-        <p>
-          Hello, my name is{" "}
-          <LinkGradient href="mailto:steffen.pedersen@live.dk">
-            Steffen Pedersen
-          </LinkGradient>
-          .
-        </p>
-        <p>
-          I work as a Frontend Developer at Jyllands-Posten and on my side
-          project Bleptek.
-        </p>
-        <p>I also sometimes write an article.</p>
-      </Content>
-      <ImageContent>
-        <div>
-          <Image
-            className="rounded-full drop-shadow-md"
-            priority
-            src="/images/profile.png"
-            height={300}
-            width={300}
-            alt="Steffen Pedersen"
-          />
-        </div>
-      </ImageContent>
+      <Grid>
+        <Content>
+          <p>
+            Hello, my name is{" "}
+            <LinkGradient href="mailto:steffen.pedersen@live.dk">
+              Steffen Pedersen
+            </LinkGradient>
+            .
+          </p>
+          <p>
+            I work as a Frontend Developer at Jyllands-Posten and on my side
+            project Bleptek.
+          </p>
+          <p>I also sometimes write an article.</p>
+        </Content>
+        <ImageContent>
+          <div>
+            <Image
+              className="rounded-full drop-shadow-md"
+              priority
+              src="/images/profile.png"
+              height={300}
+              width={300}
+              alt="Steffen Pedersen"
+            />
+          </div>
+        </ImageContent>
+      </Grid>
     </Layout>
   );
 }
