fe996c7216ebad00aea99e9b65e9863b675f2119
steffenpedersen <steffen.pedersen@live.dk>
2021-11-08 18:52:44 +0100

    Add new emoji

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -65,7 +65,7 @@ export default function Layout({
       <Head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥳</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
         />
         <script
           async
